687312da4084cd082b7ace1d42d50cb44c00cdca
Hamza Dakik <hamzadakik96@gmail.com>
2025-02-16 01:52:38 +0100

    fix netlify build issue

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { navigate } from "gatsby";
-import Layout from "./src/components/layout/layout.js";
+import Layout from "./src/components/layout/layout";
 
 import { PrismicPreviewProvider } from "gatsby-plugin-prismic-previews";
 
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "./src/components/layout/layout.js";
+import Layout from "./src/components/layout/layout";
 
 import { PrismicPreviewProvider } from "gatsby-plugin-prismic-previews";
 
